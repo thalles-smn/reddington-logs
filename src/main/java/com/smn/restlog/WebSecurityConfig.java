0244b658d7a8c5576d8e78a3cbcfff7d6edeadfa
Thalles Mendes <thalles@smnti.com.br>
2018-02-14 17:41:00 -0200

    Autenticação JWT implementada

---
+++ src/main/java/com/smn/restlog/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.smn.restlog;
+
+import com.smn.restlog.security.JWTAuthenticationFilter;
+import com.smn.restlog.security.JWTLoginFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore( new JWTLoginFilter("/login", authenticationManager()),
+                    UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore( new JWTAuthenticationFilter(),
+                    UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("1234")
+                .roles("ADMIN");
+
+    }
+
+
+}
